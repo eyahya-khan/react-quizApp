42553e2747c307dc704627f7b17d49a0fa755f3b
eyahya-khan <eyahyakhan@gmail.com>
2023-01-04 16:46:28 +0100

    Add questions

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,129 +1,118 @@
-import React,{useState} from 'react'
-import "../App.css"
-import Progress from './Progress';
-import Answers from './Answers'
-import Question from './Question'
-import { questions } from './Questions';
+import React, { useState } from "react";
+import "../App.css";
+import Progress from "./Progress";
+import Answers from "./Answers";
+import Question from "./Question";
+import { questions } from "./Questions";
 
 function Home() {
-    const [currentQuestion, setCurrentQuestion] = useState(0);
-    const [currentAnswer, setCurrentAnswer] = useState("");
-    const [answers, setAnswers] = useState([]);
-    const [error, setError] = useState("");
-    const [showReults, setShowResults] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [currentAnswer, setCurrentAnswer] = useState("");
+  const [answers, setAnswers] = useState([]);
+  const [error, setError] = useState("");
+  const [showReults, setShowResults] = useState(false);
 
-    const question = questions[currentQuestion];
-    // when click on option text (A, B, C, D)
-    const handleClick = (e) => {
-      setCurrentAnswer(e.target.value);
-      // when select an option error message will empty
-      setError("");
-    };
-    // error
-    const renderError = () => {
-      if (!error) {
-        return;
-      }
-      return <div className="error">{error}</div>;
-    };
+  const question = questions[currentQuestion];
+  const handleClick = (e) => {
+    setCurrentAnswer(e.target.value);
+    setError("");
+  };
+  const renderError = () => {
+    if (!error) {
+      return;
+    }
+    return <div className="error">{error}</div>;
+  };
 
-    // const counter = (question, answer)=> {
-    //     let counter = 1
-    //     for(let i=0; i<question.length; i++){
-    //       if (question.correct_answer === answer.answer) {
-    //           return counter++
-    //         } 
-    //     }
-    // }
-
-    // Result is correct or falied
-    const renderResultMark = (question, answer) => {
-          if (question.correct_answer === answer.answer) {
-            return <span className="correct">Correct</span>;
-          } else {
-            return <>
-            <span className="failed"> Incorrect</span>
-            <div className='correct-answer'>
-            <span>You select: {answer.answer} - Correct answer is: {question.correct_answer}</span>
-            </div>
-            </>
-          }
-    };
-    // show the result with question
-    const renderResultData = () => {
-      return answers.map((answer) => {
-        const question = questions.find(
-          (question) => question.id === answer.questionId
-        );
-        return (
-            <>
-            {/* <div>{counter(question,answer)}</div> */}
-            <div key={question.id}>
-            Question {question.id}: {renderResultMark(question, answer)}
-          </div>
-          </>
-        );
-      });
-    };
-  
-    const next = () => {
-      const answer = {
-        questionId: question.id,
-        answer: currentAnswer,
-      };
-      if (!currentAnswer) {
-        setError("Please select an option");
-        return;
-      }
-      answers.push(answer);
-      setAnswers(answers);
-      setCurrentAnswer("");
-      if (currentQuestion + 1 < questions.length) {
-        setCurrentQuestion(currentQuestion + 1);
-        return;
-      }
-      setShowResults(true);
-    };
-    // restart button everything will be empty and start from beginning
-    const restart = () => {
-      setAnswers([]);
-      setCurrentAnswer("");
-      setCurrentQuestion(0);
-      setShowResults(false);
-    };
-    // when all questions complete then it shows
-    if (showReults) {
-      return (
-        <div className="container results">
-        <h4> Your test Results </h4>
-          {/* display all question */}
-          {renderResultData()}
-          <button className="btn btn-primary" onClick={restart}>
-            Restart
-          </button>
-        </div>
-      );
+  const renderResultMark = (question, answer) => {
+    if (question.correct_answer === answer.answer) {
+      return <span className="correct">Correct</span>;
     } else {
       return (
-        <div className="container">
-          {/* current = {question.id} also works*/}
-          <Progress current={currentQuestion + 1} total={questions.length} />
-          <div className='container-ques-ans-display'>
-          <Question question={question.question} />
-          {/* display error message */}
-          <div>
-          {renderError()}
-          <Answers
-            question={question}
-            currentAnswer={currentAnswer}
-            handleClick={handleClick}
-          />
-          <button className="btn btn-primary" onClick={next}> Next</button>
+        <>
+          <span className="failed"> Incorrect</span>
+          <div className="correct-answer">
+            <span>
+              You select: {answer.answer} - Correct answer is:{" "}
+              {question.correct_answer}
+            </span>
           </div>
-        </div>
-        </div>
+        </>
       );
     }
+  };
+
+  const renderResultData = () => {
+    return answers.map((answer) => {
+      const question = questions.find(
+        (question) => question.id === answer.questionId
+      );
+      return (
+        <>
+          <div key={question.id}>
+            Question {question.id}: {renderResultMark(question, answer)}
+          </div>
+        </>
+      );
+    });
+  };
+
+  const next = () => {
+    const answer = {
+      questionId: question.id,
+      answer: currentAnswer,
+    };
+    if (!currentAnswer) {
+      setError("Please select an option");
+      return;
+    }
+    answers.push(answer);
+    setAnswers(answers);
+    setCurrentAnswer("");
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+      return;
+    }
+    setShowResults(true);
+  };
+  const restart = () => {
+    setAnswers([]);
+    setCurrentAnswer("");
+    setCurrentQuestion(0);
+    setShowResults(false);
+  };
+  if (showReults) {
+    return (
+      <div className="container results">
+        <h4> Your test Results </h4>
+        <div className="container-result-body">{renderResultData()}</div>
+        <button className="btn btn-primary" onClick={restart}>
+          Restart
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <Progress current={currentQuestion + 1} total={questions.length} />
+        <div className="container-ques-ans-display">
+          <Question question={question.question} />
+          <div>
+            {renderError()}
+            <Answers
+              question={question}
+              currentAnswer={currentAnswer}
+              handleClick={handleClick}
+            />
+            <button className="btn btn-primary" onClick={next}>
+              {" "}
+              Next
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,32 +1,35 @@
 export const questions = [
-    {
-      id: 1,
-      question: "which of the following methods cannot prevent object modification",
-      answer_a: "Object.seal()",
-      answer_b: "Object.lock()",
-      answer_c: "Object.preventExtension()",
-      answer_d: "Object.freeze()",
-      correct_answer: "b",
-    },
-    {
-      id: 2,
-      question: "which of the following is the correct way to store data in an object",
-      answer_a: "obj['class'] = 12",
-      answer_b: "obj.class = 12",
-      answer_c: "a & b",
-      answer_d: "None of above",
-      correct_answer: "c",
-    },
-    {
-      id: 3,
-      question: "If you have a function f and an object o, you can define a method named m of o with",
-      answer_a: "o.m=m.f;",
-      answer_b: "o.m=f;",
-      answer_c: "o=f.m;",
-      answer_d: "o=f;",
-      correct_answer: "a",
-    },
-    {
+  {
+    id: 1,
+    question:
+      "Which of the following methods cannot prevent object modification",
+    answer_a: "Object.seal()",
+    answer_b: "Object.lock()",
+    answer_c: "Object.preventExtension()",
+    answer_d: "Object.freeze()",
+    correct_answer: "b",
+  },
+  {
+    id: 2,
+    question:
+      "Which of the following is the correct way to store data in an object",
+    answer_a: "obj['class'] = 12",
+    answer_b: "obj.class = 12",
+    answer_c: "a & b",
+    answer_d: "None of above",
+    correct_answer: "c",
+  },
+  {
+    id: 3,
+    question:
+      "If you have a function f and an object o, you can define a method named m of o with",
+    answer_a: "o.m=m.f;",
+    answer_b: "o.m=f;",
+    answer_c: "o=f.m;",
+    answer_d: "o=f;",
+    correct_answer: "a",
+  },
+  {
     id: 4,
     question: "How much is 2+4*5+1 = ? ",
     answer_a: "23",
@@ -34,5 +37,54 @@ export const questions = [
     answer_c: "31",
     answer_d: "26",
     correct_answer: "a",
-      },
-  ];
+  },
+  {
+    id: 5,
+    question: "How is a forEach statement different from a for statement?",
+    answer_a: "Only a for statement uses a callback function.",
+    answer_b:
+      "A for statement is generic, but a forEach statement can be used only with an array.",
+    answer_c: "Only a forEach statement lets you specify your own iterator.",
+    answer_d:
+      "A forEach statement is generic, but a for statement can be used only with an array.",
+    correct_answer: "b",
+  },
+  {
+    id: 6,
+    question: "How does a function create a closure?",
+    answer_a: "It reloads the document whenever the value changes.",
+    answer_b: "It returns a reference to a variable in its parent scope.",
+    answer_c: "It completes execution without returning.",
+    answer_d: "It copies a local variable to the global scope.",
+    correct_answer: "b",
+  },
+  {
+    id: 7,
+    question:
+      "You need to match a time value such as 12:00:32. Which of the following regular expressions would work for your code?",
+    answer_a: "/[0-9]{2,}:[0-9]{2,}:[0-9]{2,}/",
+    answer_b: "/[0-9]+:[0-9]+:[0-9]+/",
+    answer_c: "/dd:dd:dd/",
+    answer_d: "/ : : /",
+    correct_answer: "c",
+  },
+  {
+    id: 8,
+    question:
+      "Which property references the DOM object that dispatched an event?",
+    answer_a: "self",
+    answer_b: "object",
+    answer_c: "source",
+    answer_d: "target",
+    correct_answer: "d",
+  },
+  {
+    id: 9,
+    question: "Which method converts JSON data to a JavaScript object?",
+    answer_a: "JSON.fromString();",
+    answer_b: "JSON.parse()",
+    answer_c: "JSON.toObject()",
+    answer_d: "JSON.stringify()",
+    correct_answer: "b",
+  },
+];
